bbad3bf893c00c97af83da0c7ce02b2c02e87da6
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-09-14 07:43:59 +0330

    cache options

---
+++ src/app/products/page.tsx
@@ -0,0 +1,30 @@
+// export const fetchCache = 'default-cache';
+import { cookies } from 'next/headers';
+type Product = {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+};
+async function ProductPage() {
+	const response = await fetch('http://localhost:3001/products');
+	const products = await response.json();
+	const cookieStore = cookies();
+	const theme = cookieStore.get('theme');
+	const detailsResponse = await fetch('http://localhost:3001/products/1');
+	const details = await detailsResponse.json();
+	return (
+		<ul className='space-y-4 p-4'>
+			{products.map((product: Product) => (
+				<li key={product.id} className='p-4 bg-white shadow-md rounded-lg text-gray-700'>
+					<h2 className='text-xl font-semibold'>{product.title}</h2>
+					<p>{product.description}</p>
+					<p className='text-lg font-medium'>${product.price}</p>
+					<p>{details.price}</p>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default ProductPage;
