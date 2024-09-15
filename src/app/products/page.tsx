7e335259def7257c5d3d010cbb12fd72f7e3b9f5
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-09-15 20:25:36 +0330

    revalidate

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,12 +7,14 @@ type Product = {
 	description: string;
 };
 async function ProductPage() {
-	const response = await fetch('http://localhost:3001/products');
+	const response = await fetch('http://localhost:3001/products', {
+		next: { revalidate: 10 },
+	});
 	const products = await response.json();
 	const cookieStore = cookies();
 	const theme = cookieStore.get('theme');
-	const detailsResponse = await fetch('http://localhost:3001/products/1');
-	const details = await detailsResponse.json();
+	// const detailsResponse = await fetch('http://localhost:3001/products/1');
+	// const details = await detailsResponse.json();
 	return (
 		<ul className='space-y-4 p-4'>
 			{products.map((product: Product) => (
@@ -20,7 +22,7 @@ async function ProductPage() {
 					<h2 className='text-xl font-semibold'>{product.title}</h2>
 					<p>{product.description}</p>
 					<p className='text-lg font-medium'>${product.price}</p>
-					<p>{details.price}</p>
+					{/* <p>{details.price}</p> */}
 				</li>
 			))}
 		</ul>
